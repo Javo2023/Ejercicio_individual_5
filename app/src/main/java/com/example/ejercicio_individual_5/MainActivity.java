c4eda286f740b52003dbe0f41c9bf16ecb13e370
javo2023 <jguzmanalarcon@gmail.com>
2023-06-19 21:46:44 -0400

    terminado

--- app/src/main/java/com/example/ejercicio_individual_5/MainActivity.java
+++ app/src/main/java/com/example/ejercicio_individual_5/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.ejercicio_individual_5;
 
+import static android.app.ProgressDialog.show;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.BreakIterator;
+
+public class MainActivity<button> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListeners();
 
     }
+
+    private void initListeners() {
+        Button showToast = findViewById(R.id.buttonShowToast);
+        Button counter =findViewById(R.id.buttonCounter);
+        TextView textView = findViewById(R.id.textView);
+
+        showToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "hello toast", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        counter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer numero = Integer.parseInt(textView.getText().toString());
+                numero ++;
+
+                textView.setText((numero.toString()));
+            }
+        })  ;
+
+    }
+
 }
 
